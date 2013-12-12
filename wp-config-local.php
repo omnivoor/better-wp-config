0633448c8cfa857ae45f90b4f6d84584b44398fb
Tiziano Di Turi <tiziano@omnivoor.be>
2013-12-12 08:44:48 +0100

    base commit

---
+++ wp-config-local.php
@@ -0,0 +1,32 @@
+<?php
+/** The name of the database for WordPress */
+define('DB_NAME', 'XXXXXXXX');
+
+/** MySQL database username */
+define('DB_USER', 'root');
+
+/** MySQL database password */
+define('DB_PASSWORD', 'root');
+
+/** MySQL hostname */
+define('DB_HOST', 'localhost');
+
+/** Database Charset to use in creating database tables. */
+define('DB_CHARSET', 'utf8');
+
+/** The Database Collate type. Don't change this if in doubt. */
+define('DB_COLLATE', '');
+
+/**
+ * For developers: WordPress debugging mode.
+ *
+ * Change this to true to enable the display of notices during development.
+ * It is strongly recommended that plugin and theme developers use WP_DEBUG
+ * in their development environments.
+ */
+define('WP_DEBUG', true);
+
+if (WP_DEBUG) {
+	define( 'SCRIPT_DEBUG', true );	
+	define( 'WP_DEBUG_LOG', true );
+}
